428591914b8878de89dec31b2bba23a90b674afd
Eduardo Santos <eduardo.santos@lpgsolucoes.com.br>
2019-02-06 10:32:00 -0300

    Enumeração e Composição de Objetos

--- Course/programas.cs
+++ Course/programas.cs
@@ -496,7 +496,28 @@ namespace Course
             int year = int.Parse(monthAndYear.Substring(3));
             Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine("Department: " + worker.Department.Name);
-            Console.WriteLine("Income for " + monthAndYear + ":" + worker.Income(year,month));
+            Console.WriteLine("Income for " + monthAndYear + ":" + worker.Income(year,month).ToString("F2",CultureInfo.InvariantCulture));
+        }
+
+        public static void Comments()
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"),"Traveling to new Zealand", "I'm going to visit this wonderful country!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys",
+                "See you tomorrow", 5);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
