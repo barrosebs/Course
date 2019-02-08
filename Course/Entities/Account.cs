7af43d7a629ffb202acf7c89126ef3c79e12836b
Eduardo Santos <eduardo.santos@lpgsolucoes.com.br>
2019-02-08 13:48:52 -0300

    Herança - É uma associação entre classe e não entre objetos

---
+++ Course/Entities/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace Course.Entities
+{
+    class Account
+    {
+        public int Namber { get; private set; }
+        public string Hilder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int namber, string hilder, double balance)
+        {
+            Namber = namber;
+            Hilder = hilder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ Course/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace Course.Entities
+{
+    /// <summary>
+    /// Herança é uma Associação entre classe e não entre objetos
+    /// </summary>
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string hilder, double balance,double loanLimit) : base (number,hilder,balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
